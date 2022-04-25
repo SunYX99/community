79edde5f827d451d3bf87d00fb612c6901df4df6
SunYX99 <862624436@qq.com>
2022-04-25 16:46:43 +0800

    开发注册功能

---
+++ src/test/java/community/MailTests.java
@@ -0,0 +1,29 @@
+package community;
+
+import community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+    @Autowired
+    private TemplateEngine templateEngine; //thymeleaf模板引擎
+
+    @Test
+    void testMail(){
+        mailClient.sendMail("862624436@qq.com","Test邮箱发送","Welcome...");
+    }
+
+    @Test
+    void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username","sunyuxiang");
+        String content = templateEngine.process("/mail/demo", context);
+        mailClient.sendMail("862624436@qq.com","Test html发送",content);
+    }
+}
